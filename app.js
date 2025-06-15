b1cde2f3279387fdf62f34dce2bb02b907420417
Zemile30 <zemile.nesirli@gmail.com>
2025-06-15 17:34:09 +0000

    js

--- app.js
+++ app.js
@@ -8,10 +8,30 @@ $(document).ready(function () {
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
-                console.log(data);
-                
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("myWeather").val("")
+                console.log(data);    
             }
+            myweather()
         }
+        
+    })
+
+    $(".btn").click(function (e) { 
+        e.preventDefault()
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("myWeather").val("")
+                console.log(data);    
+            }
+            myweather()
         
     });
 
