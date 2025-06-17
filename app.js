7c72ec7b6f0494306a24eeaabfc86630a462d7a8
Zemile30 <zemile.nesirli@gmail.com>
2025-06-17 16:17:35 +0000

    link

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     $(document).on('keypress' , function (e) { 
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("myWeather").val()}&appid=513b194a4d4d0e2122ea82011bd4137f&units=metric`
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
@@ -25,10 +25,10 @@ $(document).ready(function () {
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
-                $("#city").html(data.name)
-                $("#temp").html(data.main.temp)
-                $("#feels").html(data.main.feels_like)
-                $("myWeather").val("")
+                // $("#city").html(data.name)
+                // $("#temp").html(data.main.temp)
+                // $("#feels").html(data.main.feels_like)
+                // $("myWeather").val("")
                 console.log(data);    
             }
             myweather()
