e7c61e2dbb40fe7c1ba3c5c52b46d2ebeda91211
Zemile30 <zemile.nesirli@gmail.com>
2025-06-22 17:52:27 +0000

    correct

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
 
     $(document).on('keypress' , function (e) { 
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("myWeather").val()}&appid=513b194a4d4d0e2122ea82011bd4137f&units=metric`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
                 $("#city").html(data.name)
                 $("#temp").html(data.main.temp)
                 $("#feels").html(data.main.feels_like)
-                $("myWeather").val("")
+                $("#myWeather").val("")
                 console.log(data);    
             }
             myweather()
@@ -19,16 +19,16 @@ $(document).ready(function () {
         
     })
 
-    $(".btn").click(function (e) { 
+    $("#btn").click(function (e) { 
         e.preventDefault()
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
-                // $("#city").html(data.name)
-                // $("#temp").html(data.main.temp)
-                // $("#feels").html(data.main.feels_like)
-                // $("myWeather").val("")
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("#myWeather").val("")
                 console.log(data);    
             }
             myweather()
